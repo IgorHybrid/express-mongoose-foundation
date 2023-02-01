31befcdc81af1aaa806926fb4f11920a333c0996
Igor Iglesias <rogih_95@hotmail.com>
2023-02-02 00:43:50 +0100

    :sparkles: Connect to db

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { connectDB } from './config/db';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -7,8 +8,15 @@ app.get("/",(req, res) => {
     res.send('Hello World!');
 });
 
-const server = app.listen(
-    PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
+(async () => {
+    try {
+        await connectDB();
+        app.listen(
+            PORT, () => {
+                console.log(`Server is running on port ${PORT}`);
+            }
+        )
+    } catch (error) {
+        console.error(error);
     }
-)
+})();
