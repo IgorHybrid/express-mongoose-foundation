127bf164205677b289f9d8959af09e05077dea97
Igor Iglesias <rogih_95@hotmail.com>
2023-02-02 16:46:00 +0100

    :construction: Add login

--- src/controllers/auth/index.ts
+++ src/controllers/auth/index.ts
@@ -14,3 +14,23 @@ export const register = async (req:Request, res:Response, next:any) => {
         console.error(error);
     }
 };
+
+// TODO: Add session storage with tokens
+export const login =async (req:Request, res:Response, next:any) => {
+    const { username, email, password } = req.body;
+    try {
+        const user:IUser | null = await User.findOne({$or: [{email},{username}]}).select('+password');
+        if (!user) {
+            return res.status(401).send({msg: 'Invalid username.'});
+        }
+
+        const isMatch:boolean = await user.matchPassword(password);
+        if (!isMatch) {
+            return res.status(401).send({msg: 'Invalid password.'});
+        }
+
+        return res.status(200).send({_id: user._id});
+    } catch (error) {
+        console.error(error);
+    }
+}
--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -4,5 +4,6 @@ import { authController } from '../../controllers'
 const router = express.Router();
 
 router.route('/register').post(authController.register);
+router.route('/login').post(authController.login);
 
 export default router;
