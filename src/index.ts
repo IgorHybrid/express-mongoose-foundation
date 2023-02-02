8a3e4fed0351b9b76d42ab8709d47fbd8839676b
Igor Iglesias <rogih_95@hotmail.com>
2023-02-02 10:05:05 +0100

    :sparkles: Insert users to db

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,29 @@
 import express from 'express';
+import { json } from 'body-parser';
 import { connectDB } from './config/db';
+import User, { IUser } from './modules/User';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/",(req, res) => {
+app.use(json());
+
+app.get("/", async (req, res) => {
     res.send('Hello World!');
+});
+
+app.post("/auth/register", async (req, res) => {
+    const { username, email, password } = req.body;
+    try {
+        const user:IUser = await User.create({
+            username,
+            email,
+            password
+        });
+        return res.status(201).send({msg: "User created" })
+    } catch (error) {
+        console.error(error);
+    }
 });
 
 (async () => {
