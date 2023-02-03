cb65856084886a542504a64e19a53ee5fa4056cd
Igor Iglesias <rogih_95@hotmail.com>
2023-02-03 12:36:11 +0100

    :bug: Show error message in console

--- src/middleware/error404.ts
+++ src/middleware/error404.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express"
 
 export const error404 = (req:Request, res:Response, next:NextFunction) => {
+    res.locals.errorMessage = 'Page not found';
     res.status(404).send({message: 'Page not found'});
 }
