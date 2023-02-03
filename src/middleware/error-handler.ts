864a4ab8dfc3a23eb96d2f04adaf92f58232fa36
Igor Iglesias <rogih_95@hotmail.com>
2023-02-03 12:36:11 +0100

    :bug: Show error message in console

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -1,6 +1,8 @@
 import { Response, Request, NextFunction } from "express"
 
 export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+    res.locals.errorMessage = err.message;
+
     // custom application error
     if (typeof (err) === 'string') {
         // custom application error
