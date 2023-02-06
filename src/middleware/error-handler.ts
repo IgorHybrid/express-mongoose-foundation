91bef1fa814e3a1751607727935d26b24635c8e9
Igor Iglesias <rogih_95@hotmail.com>
2023-02-06 16:43:56 +0100

    :sparkles: Add new error types

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -8,6 +8,14 @@ export const errorHandler = (err: any, req: Request, res: Response, next: NextFu
         return res.status(400).json({ message: err.message });
     }
 
+    if (err.name === 'Unauthorized') {
+        return res.status(401).json({ message: err.message });
+    }
+
+    if (err.name === 'Forbidden') {
+        return res.status(403).json({ message: err.message });
+    }
+
     // default to 500 server error
     // console.error(err);
     return res.status(500).json({ message: err.message });
