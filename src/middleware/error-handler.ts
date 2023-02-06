839324e0bdc7f248cc9e07a9ed178ffd28c606b2
Igor Iglesias <rogih_95@hotmail.com>
2023-02-06 23:33:13 +0100

    :bulb: Remove comment

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -3,7 +3,6 @@ import { Response, Request, NextFunction } from "express"
 export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
     res.locals.errorMessage = err.message;
 
-    // mongoose validation error
     if (err.name === 'ValidationError') {
         return res.status(400).json({ message: err.message });
     }
