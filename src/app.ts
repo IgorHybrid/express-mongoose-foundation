fb69d9285e936433720d7fcdbb3c09d428b01cfb
Igor Iglesias <rogih_95@hotmail.com>
2023-02-07 12:44:55 +0100

    :art: Prepare server for testing

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,11 @@ import 'dotenv/config';
 import express from 'express';
 import passport from 'passport';
 import { json } from 'body-parser';
-import { connectDB } from './config/db';
 import routes from './routes';
 import morgan from './utils/logger/morgan';
 import { error404Handler, errorHandler, authMiddleware } from './middleware';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(morgan.successHandler);
 app.use(morgan.errorHandler);
@@ -25,15 +23,4 @@ app.use('/', routes);
 app.use(error404Handler);
 app.use(errorHandler);
 
-(async () => {
-    try {
-        await connectDB();
-        app.listen(
-            PORT, () => {
-                console.log(`Server is running on port ${PORT}`);
-            }
-        )
-    } catch (error) {
-        console.error(error);
-    }
-})();
+export default app;
