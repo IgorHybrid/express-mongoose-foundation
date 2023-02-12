fd9c3c934d159ab4456b4f8200303fd125fb6e68
Igor Iglesias <rogih_95@hotmail.com>
2023-02-12 13:22:53 +0100

    :art: Improve error handler

---
+++ src/middleware/errors/error-handler.ts
@@ -0,0 +1,31 @@
+import { Response, Request, NextFunction } from "express";
+import ApiError from "../../utils/error";
+
+export const errorConverter = (err: any, req: Request, res: Response, next: NextFunction) => {
+    let error = err;
+    if (!(error instanceof ApiError)) {
+        const message: string = error.message || 'Server internal error';
+        error = new ApiError(500, message, false, err.stack);
+    }
+    next(error);
+};
+
+export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+    const { statusCode, message } = err;
+    // if (process.env.env === 'production' && !err.isOperational) {
+    //     statusCode = 500;
+    //     message = 'Internal Server Error';
+    // }
+
+    res.locals.errorMessage = err.message;
+
+    const response = {
+        code: statusCode,
+        message,
+        stack: err.stack
+    }
+
+    //console.error(err);
+
+    res.status(statusCode).send(response);
+};
