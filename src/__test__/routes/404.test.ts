970eaeba91d4c1a01b30910628e54296ac1d75de
Igor Iglesias <rogih_95@hotmail.com>
2023-02-12 16:27:33 +0100

    :white_check_mark: Increase tests coverage

---
+++ src/__test__/routes/404.test.ts
@@ -0,0 +1,16 @@
+import request from 'supertest';
+import app from '../../app';
+
+describe('Not Found routes', () => {
+    describe('GET /invalid-route', () => {
+        test('return 404 Not Found', async () => {
+            const res = await request(app).get('/invalid-route').expect(404);
+
+            expect(res.body).toMatchObject({
+                code: 404,
+                message: 'Page not found',
+                stack: expect.anything()
+            })
+        });
+    });
+}); 
